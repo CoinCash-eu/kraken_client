57c8da677622d2c2cf19713677c6fddfaac9475b
David Debreczeni <david.debreczeni@gmail.com>
2020-04-21 18:07:34 +0200

    add DepositStatus and WithdrawStatus private endpoints and handle their response types properly

--- lib/kraken_client/endpoints/private.rb
+++ lib/kraken_client/endpoints/private.rb
@@ -6,31 +6,38 @@ module KrakenClient
         url      = config.base_uri + url_path(endpoint_name)
         response = request_manager.call(url, endpoint_name, args)
 
-        if response.is_a?(Array) && response.first.match(/^E[\w]+:/)
+        case
+        when response.is_a?(Hash)
+          response.with_indifferent_access
+        when response.is_a?(Array) && response.first.is_a?(Hash)
+          response.map(&:with_indifferent_access)
+        when response.is_a?(Array) && response.first.is_a?(String) && response.first.match(/^E[\w]+:/)
           fail ErrorResponse.new(response.first)
-        elsif response == "error"
+        when response == "error"
           fail ErrorResponse.new(response)
         else
-          response.with_indifferent_access
+          response
         end
       end
 
       def data
         {
-          :Balance       => :balance,
-          :TradeBalance  => :trade_balance,
-          :OpenOrders    => :open_orders,
-          :ClosedOrders  => :closed_orders,
-          :QueryOrders   => [:query_orders,  params: [:txid]],
-          :TradesHistory => :trades_history,
-          :QueryTrades   => [:query_trades,  params: [:txid]],
-          :OpenPositions => :open_positions, params: [:txid],
-          :Ledgers       => :ledgers,
-          :QueryLedgers  => [:query_ledgers, params: [:id]],
-          :TradeVolume   => :trade_volume,
-          :AddOrder      => [:add_order,     params: [:pair, :type, :ordertype, :volume]],
-          :CancelOrder   => [:cancel_order,  params: [:txid]],
-          :Withdraw      => [:withdraw,      params: [:asset, :key, :amount]]
+          :Balance        => :balance,
+          :TradeBalance   => :trade_balance,
+          :OpenOrders     => :open_orders,
+          :ClosedOrders   => :closed_orders,
+          :QueryOrders    => [:query_orders,    params: [:txid]],
+          :TradesHistory  => :trades_history,
+          :QueryTrades    => [:query_trades,    params: [:txid]],
+          :OpenPositions  => :open_positions,   params: [:txid],
+          :Ledgers        => :ledgers,
+          :QueryLedgers   => [:query_ledgers,   params: [:id]],
+          :TradeVolume    => :trade_volume,
+          :AddOrder       => [:add_order,       params: [:pair, :type, :ordertype, :volume]],
+          :CancelOrder    => [:cancel_order,    params: [:txid]],
+          :Withdraw       => [:withdraw,        params: [:asset, :key, :amount]],
+          :DepositStatus  => [:deposit_status,  params: [:asset]],
+          :WithdrawStatus => [:withdraw_status, params: [:asset]],
         }
       end
 
